db09e14bbc5a5dc1bea4d79053c60d8be69f1796
joao.felipe <joao.felipe@propig.com.br>
2024-05-12 10:21:10 -0300

    [ feat ] -> flask api service

---
+++ api/app.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+import json
+import os
+import random
+import uuid
+from flask import Flask
+import redis
+
+
+def get_payment():
+    return {
+        'url': os.getenv("WEBHOOK_ADDRESS", ""),
+        'webhookId': uuid.uuid4().hex,
+        'data': {
+            'id': uuid.uuid4().hex,
+            'payment': f"PY-{''.join((random.choice('abcdxyzpqr').capitalize() for i in range(5)))}",
+            'event': random.choice(["accepted", "completed", "canceled"]),
+            'created': datetime.now().strftime("%d/%m/%Y, %H:%M:%S"),
+        }
+    }
+
+
+redis_address = os.getenv("REDIS_ADDRESS", "")
+host, port = redis_address.split(":")
+port = int(port)
+
+redis_connection = redis.StrictRedis(host=host, port=port)
+
+app = Flask(__name__)
+
+
+@app.route('/payment')
+def payment():
+    webhook_payload_json = json.dumps(get_payment())
+
+    
+    redis_connection.publish('payments', webhook_payload_json)
+
+    return webhook_payload_json
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
